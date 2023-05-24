e3551ce73803518d3ed451e4aba087c6ebb19b60
Jairo Barros <jbarros@platformscience.com>
2023-05-23 21:43:30 -0700

    Initial Commit

---
+++ food-rotation/food_rotationApp.swift
@@ -0,0 +1,17 @@
+//
+//  food_rotationApp.swift
+//  food-rotation
+//
+//  Created by Jairo Barros on 5/23/23.
+//
+
+import SwiftUI
+
+@main
+struct food_rotationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
